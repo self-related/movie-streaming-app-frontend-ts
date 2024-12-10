0daf19e69433bb0cfa42655e6f38e570bdbaafe4
Nekit <nick.misiura@yandex.ru>
2024-12-10 17:13:46 +0300

    create left navbar; change top navbar and footer to be inside main; css BEM naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import './App.css'
+import NavBarLeft from './components/NavBarLeft';
 
 function App() {
 
   return (
     <>
-        <header>header</header>
-        <main>
-            <Outlet />
+        <main className="split">
+            <NavBarLeft />
+            <div className="content">
+                <div>Header</div>
+                <Outlet />
+                <div>footer</div>
+            </div>
         </main>
-        <footer>footer</footer>
     </>
   );
 }
---
+++ src/components/NavBarLeft.jsx
@@ -0,0 +1,10 @@
+import "./NavBarLeft.css";
+
+export default function NavBarLeft() {
+
+    return (
+        <nav className="navbar-left">
+            NavBarLeft
+        </nav>
+    );
+}
