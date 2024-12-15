3a8f5d646da33923afd178263a0c9b4c94b864b5
Nekit <nick.misiura@yandex.ru>
2024-12-15 12:19:43 +0300

    add links to the left panel; add icons; decorate the left panel; border-box size for all css elements

--- src/components/NavBarLeft.jsx
+++ src/components/NavBarLeft.jsx
@@ -1,10 +1,25 @@
+import { Link } from "react-router-dom";
 import "./NavBarLeft.css";
 
 export default function NavBarLeft() {
 
     return (
         <nav className="navbar_left">
-            NavBarLeft
+            <Link to="/welcome" className="navbar_left__logo"> <img src="/logo.svg" alt="logo" height={22}/> WATCH</Link>
+            <Link to="/" className="navbar_left__link"> <img src="/icons/home.svg" /> Home</Link>
+            <Link to="/favorites" className="navbar_left__link"> <img src="/icons/favorites.svg" /> Favorites</Link>
+            <Link to="/trending" className="navbar_left__link"> <img src="/icons/trending.svg" /> Trending</Link>
+            <Link to="/" className="navbar_left__link"> <img src="/icons/coming-soon.svg" /> Coming Soon</Link>
+
+            <div className="navbar_left__wrapper_div">
+                <Link to="/" className="navbar_left__link"> <img src="/icons/community.svg" /> Community</Link>
+                <Link to="/" className="navbar_left__link"> <img src="/icons/social.svg" /> Social</Link>
+            </div>
+
+            <div className="navbar_left__wrapper_div">
+                <Link to="/" className="navbar_left__link"> <img src="/icons/settings.svg" /> Settings</Link>
+                <Link to="/" className="navbar_left__link"> <img src="/icons/logout.svg" /> Logout</Link>
+            </div>
         </nav>
     );
 }
