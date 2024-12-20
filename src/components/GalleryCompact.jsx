f9f773c871b62fb052d20d151e6aaa24c29432c8
Nekit <nick.misiura@yandex.ru>
2024-12-20 15:07:44 +0300

    change json structure

--- src/components/GalleryCompact.jsx
+++ src/components/GalleryCompact.jsx
@@ -11,12 +11,12 @@ import "./GalleryCompact.css"
 */
 export default function GalleryCompact({movies}) {
 
-    const moviesList = movies?.map(({image, description}, index) => (
+    const moviesList = movies?.map(({image, info}, index) => (
         <div className="thumbnail_compact" key={index}>
-            <img src={image} alt={description.name} className="thumbnail_compact__image" />
+            <img src={image} alt={info.name} className="thumbnail_compact__image" />
             <div className="thumbnail_compact__info">
-                <h3>{description.name}</h3>
-                <p>{description.year} | {description.genre}</p>
+                <h3>{info.name}</h3>
+                <p>{info.year} | {info.genre}</p>
             </div>
         </div>
     ));
