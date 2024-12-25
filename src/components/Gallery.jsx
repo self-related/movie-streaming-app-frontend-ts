961e8ee370153bd24bfcd5970c0887159c95aa77
Nekit <nick.misiura@yandex.ru>
2024-12-25 12:17:20 +0300

    add window scrolling while clicking a thumbnail

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,9 +2,19 @@ import "./Gallery.css";
 
 export default function Gallery({movies, large}) {
     const thumbnailType = large ? "large" : "compact"; // аттрибут для широких/узких превью
+    
+    const handleThumbnailClick = (event) => {
+        const coordY = event.target.getBoundingClientRect().top;
+        scrollBy({
+            top: coordY,
+            behavior: "smooth"
+        });
+
+    };
+
 
     const moviesList = movies?.map(({image, info}, index) => (
-        <div className={`thumbnail thumbnail--${thumbnailType}`} key={index}>
+        <div className={`thumbnail thumbnail--${thumbnailType}`} key={index} onClick={handleThumbnailClick}>
             <img src={image} alt={info.name} className="thumbnail__image" />
             <div className="thumbnail__info">
                 <h3>{info.name}</h3>
